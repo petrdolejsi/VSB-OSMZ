ee3443b5e1de2e1fb36577bbb46ea540020e29f6
Petr Dolejší <dolejsi.petr@gmail.com>
2019-03-01 17:46:23 +0100

    Revert "exercise 2"
    This reverts commit 7d8247c9e1088437980d0b77a6df29e71eb48f21.

--- HTTPServer/app/src/main/java/com/kru13/httpserver/ClientThread.java
+++ HTTPServer/app/src/main/java/com/kru13/httpserver/ClientThread.java
@@ -14,12 +14,10 @@ import java.io.OutputStreamWriter;
 import java.net.FileNameMap;
 import java.net.Socket;
 import java.net.URLConnection;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ClientThread extends Thread
-{
+public class ClientThread extends Thread {
 
     Socket s;
     boolean bRunning;
@@ -68,8 +66,7 @@ public class ClientThread extends Thread
             List<String> request = new ArrayList<>();
             String tmp;
 
-            while (!(tmp = in.readLine()).isEmpty())
-            {
+            while (!(tmp = in.readLine()).isEmpty()) {
                 request.add(tmp);
                 Log.d("SERVER CLIENT REQUEST", tmp);
             }
@@ -79,43 +76,29 @@ public class ClientThread extends Thread
 
             File file = new File(Environment.getExternalStorageDirectory().getPath() + "/HttpServer" + fileName);
 
-            if (file.isDirectory())
-            {
+            if (file.isDirectory()) {
                 Log.d("FILE TYPE", "Folder");
                 String html = "";
-                html += "<html><head><title>Index of " + fileName + "</title></head><body><h1>Index of " + fileName + "</h1><table><tr><th width=\"200\">Name</th><th width=\"100\">Type</th><th width=\"170\">Last modified</th><th width=\"70\">Size</th></tr>";
+                html += "<html><body><h1>Index of " + fileName + "</h1><table><tr><th width=\"100\">Type</th><th width=\"200\">Name</th><th width=\"70\">Size</th></tr>";
                 File[] listOfFiles = file.listFiles();
 
                 if (!fileName.equals("/"))
                 {
-                    html += "<tr><td><a href=\"../\">Parent Directory</a></td><td></td><td></td><td></td></tr>";
+                    html += "<tr><td></td><td><a href=\"../\">Parent Directory</a></td><td></td></tr>";
                 }
-                String folders = "";
-                String files = "";
-                SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
-                for (File tempFile : listOfFiles)
-                {
+                for (File tempFile : listOfFiles) {
                     if (tempFile.isDirectory())
                     {
-                        folders += "<tr><td><a href=\"" + tempFile.getName() + "\">" + tempFile.getName() + "/</a></td><td>-</td><td>-</td><td>-</td></tr>";
-                    }
-
-                    if (tempFile.isFile())
-                    {
-                        int i = tempFile.getName().lastIndexOf('.');
-
-                        files += "<tr><td><a href=\"" + fileName;
-
-                        if (!fileName.equals("/"))
-                        {
-                            files += "/";
-                        }
-
-                        files += tempFile.getName() + "\">" + tempFile.getName() + "</a></td><td>" + tempFile.getName().substring(i + 1) + "</td><td>" + sdf.format(tempFile.lastModified()) + "</td><td>" + getFormattedSize(tempFile.length()) + "</td></tr>";
+                        html += "<tr><td></td><td><a href=\"" + tempFile.getName() + "\">" + tempFile.getName() + "</a></td><td></td></tr>";
                     }
                 }
-                html += folders + files;
+                for (File tempFile : listOfFiles) {
+                    if (tempFile.isFile()) {
+                        html += "<tr><td>" + getMimeType(tempFile.getName()) + "</td><td><a href=\"" + tempFile.getName() + "\">" + tempFile.getName() + "</a></td><td>" + getFormattedSize(tempFile.length()) + "</td></tr>";
+                    }
+                }
                 html += "</table></body></html>\n";
+
 
                 out.write("HTTP/1.1 200 OK");
                 out.write("Content-type: text/html\n");
@@ -124,62 +107,56 @@ public class ClientThread extends Thread
                 out.write(html);
                 out.flush();
 
-            }
-            else if (file.exists())
-            {
+            } else if (file.exists()) {
                 Log.d("FILE TYPE", "File");
                 out.write("HTTP/1.1 200 OK");
                 int i = fileName.lastIndexOf('.');
                 String extension = "";
-
-                if (i > 0)
-                {
+                if (i > 0) {
                     extension = fileName.substring(i + 1);
                 }
+                if (extension.equals("jpg") || extension.equals("jpeg") || extension.equals("png") || extension.equals("html") || extension.equals("htm")) {
+                    out.write("Connection: close\n");
+                    out.write("Content-Type: " + getMimeType(fileName) + "\n");
+                    out.write("Content-Length:" + String.valueOf(file.length()) + "\n");
+                    out.write("\n");
+                    out.flush();
 
-                out.write("Connection: close\n");
-                out.write("Content-Type: " + getMimeType(fileName) + "\n");
-                out.write("Content-Length:" + String.valueOf(file.length()) + "\n");
-                out.write("\n");
-                out.flush();
+                    FileInputStream fis = new FileInputStream(file);
+                    byte buffer[] = new byte[1024];
+                    int len;
+                    while ((len = fis.read(buffer, 0, 1024)) > 0) {
+                        o.write(buffer, 0, len);
+                    }
+                    out.flush();
+                    s.close();
 
-                FileInputStream fis = new FileInputStream(file);
-                byte buffer[] = new byte[1024];
-                int len;
-                while ((len = fis.read(buffer, 0, 1024)) > 0)
-                {
-                    o.write(buffer, 0, len);
+                } else {
+                    out.write("\n");
+                    out.write("<html><body><h1>Unknown extension</h1></body></html>\n");
                 }
                 out.flush();
-                s.close();
-            }
-            else
-            {
+            } else {
                 Log.d("FILE TYPE", "404");
                 out.write("HTTP/1.1 404 Not Found\n");
                 out.write("Content-type: text/html\n");
                 out.write("\n");
-                out.write("<html><head><title>Error 404</title></head><body><h1>Error 404 - file not found</h1></body></html>\n");
+                out.write("<html><body><h1>Error 404</h1></body></html>\n");
                 out.flush();
             }
 
             s.close();
             Log.d("SERVER", "Socket Closed");
         }
-        catch (IOException e)
-        {
+        catch (IOException e) {
             if (s != null && s.isClosed())
-            {
                 Log.d("SERVER", "Normal exit");
-            }
-            else
-            {
+            else {
                 Log.d("SERVER", "Error");
                 e.printStackTrace();
             }
         }
-        finally
-        {
+        finally {
             s = null;
             bRunning = false;
         }
--- HTTPServer/app/src/main/java/com/kru13/httpserver/HttpServerActivity.java
+++ HTTPServer/app/src/main/java/com/kru13/httpserver/HttpServerActivity.java
@@ -1,34 +1,24 @@
 package com.kru13.httpserver;
 
-import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.app.Activity;
-import android.text.format.Formatter;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
-public class HttpServerActivity extends Activity implements OnClickListener
-{
+public class HttpServerActivity extends Activity implements OnClickListener{
 
 	private SocketServer s;
 	
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_http_server);
         
         Button btn1 = (Button)findViewById(R.id.button1);
         Button btn2 = (Button)findViewById(R.id.button2);
-
-		TextView IpAddress = (TextView)findViewById(R.id.ipAddress);
-		WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
-		String ipAddress = Formatter.formatIpAddress(wifiManager.getConnectionInfo().getIpAddress());
-		IpAddress.setText("Your Device IP Address: " + ipAddress);
          
         btn1.setOnClickListener(this);
         btn2.setOnClickListener(this);
@@ -37,8 +27,7 @@ public class HttpServerActivity extends Activity implements OnClickListener
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
+    public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.http_server, menu);
         return true;
@@ -48,32 +37,20 @@ public class HttpServerActivity extends Activity implements OnClickListener
 	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
-		if (v.getId() == R.id.button1)
-		{
+		if (v.getId() == R.id.button1) {
 			s = new SocketServer();
 			s.start();
-
-            TextView serverState = (TextView)findViewById(R.id.serverState);
-            serverState.setText(getString(R.string.server_run_text));
-
-			Toast.makeText(this, getString(R.string.server_run_text), Toast.LENGTH_SHORT).show();
+			Toast.makeText(this, "Server running", Toast.LENGTH_SHORT).show();
 		}
-		if (v.getId() == R.id.button2)
-		{
+		if (v.getId() == R.id.button2) {
 			s.close();
-			try
-            {
+			try {
 				s.join();
-			}
-			catch (InterruptedException e)
-            {
+			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-
-			TextView serverState = (TextView)findViewById(R.id.serverState);
-			serverState.setText(getString(R.string.server_stop_text));
-
-			Toast.makeText(this, getString(R.string.server_stop_text), Toast.LENGTH_SHORT).show();
+			Toast.makeText(this, "Server stopped", Toast.LENGTH_SHORT).show();
 		}
 	}
+    
 }
